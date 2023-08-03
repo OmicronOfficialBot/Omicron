457701f30697d16d599ffed48912f29e4c1a36e6
lIess1 <141075331+lIess1@users.noreply.github.com>
2023-08-03 17:58:19 -0400

    Update errors.js

--- helpers/errors.js
+++ helpers/errors.js
@@ -1,1 +1,8 @@
+const client = require("../index");
 
+process.on("unhandledRejection", error => {
+	client.error("Unhandled promise rejection:\n" + error);
+});
+process.on("uncaughtException", error => {
+  client.error("UncaughtExceptionError:\n" + error);
+}
