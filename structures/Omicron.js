38d3a6c6f362351bf641fd853d39a95054bc7cdf
lIess1 <141075331+lIess1@users.noreply.github.com>
2023-08-03 18:17:19 -0400

    Update Omicron.js

--- structures/Omicron.js
+++ structures/Omicron.js
@@ -1,2 +1,11 @@
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const Logger = require("../helpers/logger.js");
+const Config = require("../config");
+class Omicron extends Client {
+    constructor() {
+        super({
+            intents: Config.Intents,
+        });
+
+    }
+}
