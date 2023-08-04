e3c05e464225f24bd2e535d73590f043d72f66a8
lIess1 <141075331+lIess1@users.noreply.github.com>
2023-08-03 21:17:35 -0400

    Update Omicron.js

--- structures/Omicron.js
+++ structures/Omicron.js
@@ -1,11 +1,34 @@
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const Logger = require("../helpers/logger.js");
 const Config = require("../config");
+const fs     = require("fs");
 class Omicron extends Client {
     constructor() {
         super({
             intents: Config.Intents,
         });
+        this.commands      = new Collection();
+        this.users         = new Collection();
+        this.aliases       = new Collection();
+        this.slashCommands = new Collection();
+        this.cooldowns     = new Collection();
+        this.modules       = new Collection();
+    }
+    start() {
+        // Login to Discord Client
+        this.login(Config.Token);
+        // Load Clients Base
+        this.loadCommands(Config.Directories.Commands);
+        this.loadEvents(Config.Directories.Events);
+        //this.loadDashboard();
+        //this.loadDatabase();
+    }
+    loadCommands(dir) {
+        let modules = fs.readdirSync(dir);
+        for(let module of modules) {
+            // Load Guild Modules
 
+            
+        }
     }
 }
