254eac66791b2ae1cf726a17bc8a97d164a27580
lIess1 <141075331+lIess1@users.noreply.github.com>
2023-08-04 10:22:14 -0400

    Update logger.js

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,6 +1,19 @@
+// Color Logging Module
 const color = require("ansicolor");
 
+// Logging
 class Logger {
-  constructor(client) {
-  }
+    constructor(client) {
+        this.client = client;
+    }
+    error(err) {
+        console.log(color.red("❌ [Error]!\n" + err));
+    }
+    info(txt) {
+        console.log(color.blue("ℹ️ [Info]\n" + txt));
+    }
+    warn(txt) {
+        console.log(color.yellow("⚠️ [Warning]\n" + txt));
+    }
 }
+                    
