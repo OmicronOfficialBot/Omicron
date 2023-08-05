e1bc7d50f088bed812337e93f9c68f2eb7bf7fd6
lIess1 <141075331+lIess1@users.noreply.github.com>
2023-08-05 15:31:09 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-require("./helpers/errors")();
-
-const Client = require("./structures/Omicron.js");
-const Omicron = new Client();
-Omicron.start();
-
-module.exports = Omicron;
