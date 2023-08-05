5f61ade2ab13ad005c32f5f4e1bc536975795894
lIess1 <141075331+lIess1@users.noreply.github.com>
2023-08-05 15:27:58 -0400

    Delete helpers directory

--- helpers/errors.js
+++
@@ -1,9 +0,0 @@
-const client = require("../index");
-
-// Handle Errors
-process.on("unhandledRejection", error => {
-	client.error("Unhandled promise rejection:\n" + error);
-});
-process.on("uncaughtException", error => {
-	client.error("UncaughtExceptionError:\n" + error);
-}
