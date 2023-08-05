2c05d359c8d33cf5b545adb76ef6a795f0fe9254
lIess1 <141075331+lIess1@users.noreply.github.com>
2023-08-05 15:30:27 -0400

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,16 @@
+const { GatewayIntentBits } = require("discord.js");
+
+module.exports = {
+    token: process.env.token,
+    invite: "https://discord.com/api/oauth2/authorize?client_id=1133502190477123787&permissions=70368744177655&scope=bot",
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+    ],
+    partials: "",
+    paths: {
+        commands: `${process.cwd()}/commands`,
+        events: `${process.cwd()}/events`,
+    }
+}
