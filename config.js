df7d1006c282edbb6eb0c3267085a82e379db350
lIess1 <141075331+lIess1@users.noreply.github.com>
2023-08-04 20:24:44 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,16 @@
+const { GatewayIntentBits } = require("discord.js");
+
 module.exports = {
-    Token: "MTEzMzUwMjE5MDQ3NzEyMzc4Nw.GLT14q.4Z0Ly5cnZnXVhJJsd4GZSHM-ohp_RDSOAeJ3XA",
-    Invite: "https://discord.com/api/oauth2/authorize?client_id=1133502190477123787&permissions=70368744177655&scope=bot",
-    Intents: [
+    token: process.env.token,
+    invite: "https://discord.com/api/oauth2/authorize?client_id=1133502190477123787&permissions=70368744177655&scope=bot",
+    intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildMessageReactions,
     ],
-    Partials: "",
-    CommandsDir: `${process.cwd()}/src/commands`,
-    EventsDir: `${process.cwd()}/src/events`,
+    partials: "",
+    paths: {
+        commands: `${process.cwd()}/commands`,
+        events: `${process.cwd()}/events‘,
+    }
 }
