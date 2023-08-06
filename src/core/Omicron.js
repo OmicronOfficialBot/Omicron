01af05ff5e7645e44cd92c3b8644bc471ef216d7
lIess1 <141075331+lIess1@users.noreply.github.com>
2023-08-06 16:44:30 -0400

    Update Omicron.js

--- src/core/Omicron.js
+++ src/core/Omicron.js
@@ -1,4 +1,6 @@
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const fs = require("fs");
+const glob = require("glob");
 const database = require("./database");
 const logger = require("./logger");
 const config = require("../config");
