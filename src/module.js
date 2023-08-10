350b279547e07020e9ca3282f4d433cf55105bcd
lIess1 <141075331+lIess1@users.noreply.github.com>
2023-08-09 20:51:35 -0400

    Create module.js

---
+++ src/module.js
@@ -0,0 +1,19 @@
+class Module {
+    constructor(client, arg) {
+        this.client = client;
+        this.module = arg.module;
+        this.enabled = arg.enabled || false;
+        this.log = arg.log;
+        if(this.log) client.logger.info(`[${this.module}Module] Successfully loaded module.
+    }
+}
+
+/* class AutoModextends Module {
+   constructor(client) {
+       super(client, {
+           module: "automod",
+           enabled: true,
+           log: true,
+       });
+   }
+*/
