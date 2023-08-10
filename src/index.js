8d9d369b3910fc494fa6c8fcd3aa99b91d2e3535
lIess1 <141075331+lIess1@users.noreply.github.com>
2023-08-09 21:04:31 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 const Client = require("./core/Omicron");
 const Omicron = new Client();
+const fs = require("fs");
 Omicron.start();
+
+fs.readdirSync("./managers").then(managers => { for(let manager of managers) {if(manager.enabled) new (require(`${manager}`);}});
